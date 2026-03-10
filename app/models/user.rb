f0d40fdcdf7f57c92d82ed3e403fecd0a3010cd0
Gregor Gatej <gregor.gatej@gmail.com>
2026-03-10 06:05:32 +0100

    feat: add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 3..12 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..16 }
+  validates :age, numericality: { greater_than_or_equal_to: 15, less_than_or_equal_to: 120 }
 end
